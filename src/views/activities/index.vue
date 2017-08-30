de6225c73ff01f57e52e0fe580f22c0966482370
bb chen <bb.chen@outlook.com>
2017-08-30 13:22:50 +0800

    活动列表做分页处理

--- src/views/activities/index.vue
+++ src/views/activities/index.vue
@@ -5,7 +5,7 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="thumbnail">
-                            <img :src="activity.img" />
+                            <img :src="activity.img"/>
                             <div class="caption">
                                 <h3>
                                     {{ activity.title }}
@@ -23,6 +23,16 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-8 col-md-offset-2" style="margin-bottom: 10%; margin-top: 5%">
+            <!--<span class="demonstration">直接前往</span>-->
+            <el-pagination
+                    @current-change="handleCurrentChange"
+                    :current-page.sync="currentPage3"
+                    :page-size="page_size"
+                    layout="prev, pager, next, jumper"
+                    :total="total">
+            </el-pagination>
+        </div>
     </div>
 </template>
 
@@ -32,12 +42,34 @@
       axios.get('api/activities')
         .then(res => {
           console.log(res.data);
-          this.activities = res.data;
+          this.activities = res.data.data;
+          this.total = res.data.total;
+          this.page_size = res.data.per_page;
         })
     },
     data() {
       return {
         activities: [],
+        currentPage3: 1,
+        total: 200,
+        page_size: 10,
+      }
+    },
+    methods: {
+
+      handleCurrentChange(val)
+      {
+        axios.get('api/activities?page=' + val)
+          .then(response => {
+//            this.activities = null;
+            this.activities = [];
+            let k = response.data.data;
+            let i = 0;
+            while (k[i] != null) {
+              this.activities.push(k[i]);
+              i++;
+            }
+          })
       }
     }
 
